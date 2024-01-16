ed47b12f045d9717623517e52f0ec849b9011d1c
chosw1002@naver.com <chosw1002@naver.com>
2024-01-16 15:42:30 +0900

    fix: 경로 변경

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/test")
 public class TestController {
 	@GetMapping
 	public String testApi() {
